2e5d0dfa9f848495c7802074e7b91413f1dda139
Axel <axel_kennedy@outlook.com>
2019-01-26 17:41:13 +1000

    trying again to fix unigame thing

---
+++ CanWeGoHomeYet/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public string nextScene;
+
+    //fading in variables
+    [SerializeField]
+    private CanvasGroup fader;
+    private int framesToLoad = 30;
+
+    public static GameManager Instance;
+
+
+	// Use this for initialization
+	void Start () {
+
+		if (!Instance) {
+            Instance = this;
+        } else {
+            Destroy(this);
+        }
+
+        StartCoroutine(FadeIn());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //change to the next scene
+    public void ChangeScene() {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    //fade into the game
+    IEnumerator FadeIn() {
+
+        for (float i = 1; i > 0; i -= (1f/30f)) {
+            fader.alpha = i;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
