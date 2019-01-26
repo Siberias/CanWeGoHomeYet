b0dfe9228ef7c1967f5686815ec9144bce0966c7
Oliver Taylor <green-coats@outlook.com>
2019-01-26 17:34:52 +1000

    enemies for walking

---
+++ CanWeGoHomeYet/Assets/Scripts/enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+    public GameObject[] characters;
+    public Vector3 spawnPos;
+    private int numEnemies;
+    public int enemyDis;
+
+	// Use this for initialization
+	void Start () {
+        numEnemies = 10;
+        //enem
+        SetEnemies();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetEnemies() {
+        int addZ = 10;
+        for (int i = 0; i < numEnemies; i++) {
+            int ranX = (Random.Range(-1, 2) * 3);
+            addZ += 10;
+            Instantiate(characters[Random.Range(0, characters.Length)], new Vector3(ranX, 1, addZ), Quaternion.identity);
+            //Instantiate(characters[Random.Range(0, characters.Length)], GeneratedPos(), Quaternion.identity);
+        }
+    }
+
+    Vector3 GeneratedPos() {
+        int x = (Random.Range(-1, 2) * 3);
+        int y = 1;
+        int z =+ 10; //Random.Range(10, 70);
+        print(x + "  -  " + y + "  -  " + z);
+        return new Vector3(x, y, z);
+    }
+}
--- CanWeGoHomeYet/Assets/Scripts/walking.cs
+++ CanWeGoHomeYet/Assets/Scripts/walking.cs
@@ -6,24 +6,29 @@ public class walking : MonoBehaviour {
 
     public GameObject player;
     private int lane;
+    public bool walking;
 
 	// Use this for initialization
 	void Start () {
         //Set lane to 1 (middle)
         lane = 1;
+
+        walking = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //Move Forward
-        player.transform.Translate(new Vector3(0, 0, 10) * Time.deltaTime);
+        if (walking == true) {
+            //Move Forward
+            player.transform.Translate(new Vector3(0, 0, 10) * Time.deltaTime);
+        }
 
         //if press a move left
-        if (Input.GetKeyDown("a") && lane != 0) {
+        if (lane != 0 && (Input.GetKeyDown("a") || Input.GetKeyDown(KeyCode.LeftArrow))) {
             MoveLeft();
         }
         //if press d move right
-        if (Input.GetKeyDown("d") && lane != 2) {
+        if (lane != 2 && (Input.GetKeyDown("d") || Input.GetKeyDown(KeyCode.RightArrow))) {
             MoveRight();
         }
     }
@@ -36,5 +41,10 @@ public class walking : MonoBehaviour {
     void MoveLeft() {
         player.transform.Translate(new Vector3(-3, 0, 0));
         lane--;
+    }
+
+    //walk into enemy
+    private void OnTriggerEnter(Collider other) {
+        walking = false;
     }
 }
