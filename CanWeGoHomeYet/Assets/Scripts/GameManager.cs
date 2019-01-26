3bdea3546c74292b3f48587b534467ed9e29c6cd
Axel <axel_kennedy@outlook.com>
2019-01-26 18:20:32 +1000

    Implemented clock

--- CanWeGoHomeYet/Assets/Scripts/GameManager.cs
+++ CanWeGoHomeYet/Assets/Scripts/GameManager.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public string nextScene;
+
+    //clock variables
+    [SerializeField]
+    private Text clock;
+    private int totalMinutes;
+    private float minuteTimer;
+    private float minuteTime = 1;
 
     //fading in variables
     [SerializeField]
@@ -25,16 +33,48 @@ public class GameManager : MonoBehaviour {
         }
 
         StartCoroutine(FadeIn());
+
+        //set up clock variables
+        totalMinutes = 9 * 60;
+        minuteTimer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (minuteTimer <= Time.time) {
+
+            totalMinutes++;
+            minuteTimer = Time.time + minuteTime;
+
+            clock.text = ConvertMinutes();
+        }
 	}
 
     //change to the next scene
     public void ChangeScene() {
         SceneManager.LoadScene(nextScene);
+    }
+
+    //convert minutes to digital time as string
+    private string ConvertMinutes() {
+
+        int hours = totalMinutes / 60;
+        int minutes = totalMinutes % 60;
+        string returnTime;
+               
+        if (hours < 10) {
+            returnTime = "0" + hours + ":";
+        } else {
+            returnTime = hours + ":";
+        }
+
+        if (minutes < 10) {
+            returnTime += "0" + minutes;
+        } else {
+            returnTime += minutes;
+        }
+
+        return returnTime;
     }
 
     //fade into the game
